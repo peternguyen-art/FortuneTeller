ef1e794cf3e183d6c59f3a26af4bb1ef65b26916
Peter Nguyen <nguye5he@mail.uc.edu>
2025-10-06 21:54:14 -0400

    Initial commit

---
+++ src/FortuneTellerFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class FortuneTellerFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setTitle("Fortune Teller");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+
+    JPanel mainPnl = new JPanel();
+
+    JLabel lblNewLabel = new JLabel("Fortune Teller");
+
+
+}
